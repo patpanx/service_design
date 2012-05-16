b3fc7e287de4948cac4ea8b786016b5e231a30f2
Fabian Troxler <fabian.troxler@gmail.com>
2012-05-16 11:24:26 +0200

    first step
    rub on rails new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiceDesign::Application.config.secret_token = '715f3a782c906f5b39603b5a6c3061f6d7af3277ad5c4e0553d307af90f1c720cd601db60f2a6f64f148d16f639acc0571f8cc1fd4efad30c0309b1c0fbb6c9f'
