d95cd9cc4bb33adb11ffe7a9778cce5d00e74007
Patrick Pandolfo <info@pan-x.ch>
2012-05-17 17:29:54 +0200

    password_digest to do: bundle install rake db:migrate

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.3'
 
 gem 'sqlite3'
 gem 'json'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(:version => 20120516134649) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
