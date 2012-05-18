7bb0869fb52174771f9ea776661ee6289b5f4a86
Fabian Troxler <fabian.troxler@gmail.com>
2012-05-18 12:00:07 +0200

    mobile ansichten

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   #check if user is logged in <- this is executed every time a controller is active
-  
+  before_filter :check_mobile
   before_filter :check_login
   protect_from_forgery
   
@@ -19,9 +19,18 @@ class ApplicationController < ActionController::Base
    end
    
    def is_mobile?
-     
-     
+     if session[ :is_mobile]
+        return ( session[ :is_mobile ].to_i == 1 )
+      else
+        return ( request.user_agent.downcase =~ /iphone|android|mobile/ )
+      end
    end
+   helper_method :is_mobile
    
+   def check_mobile
+      session[ :is_mobile ] = params[ :mobile ] if params[ :mobile ]
+      request.format = :mobile if is_mobile?
+      logger.debug "---- user agent: #{request.user_agent}"
+    end
    
 end
