75b53dfb3b277c46e0d27f397886146659e33648
Fabian Troxler <fabian.troxler@gmail.com>
2012-05-18 10:11:19 +0200

    changed password to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120517204452) do
+ActiveRecord::Schema.define(:version => 20120518075155) do
 
   create_table "messages", :force => true do |t|
     t.integer  "session_id"
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(:version => 20120517204452) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "admin",      :default => false
+    t.string   "password_digest"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.boolean  "admin",           :default => false
   end
 
 end
