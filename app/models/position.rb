36d09f984fba2a013eb6aa29186d468ba22d3696
Fabian Troxler <fabian.troxler@gmail.com>
2012-05-21 13:08:34 +0200

    position

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,6 @@
 class Position < ActiveRecord::Base
   attr_accessible :accuracy, :altitude, :altitude_accuracy, :lat, :long, :timestamp, :user_id
+  
+  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
    
   has_many :messages
   has_many :sessions, :foreign_key => 'owner_id'
+  has_many :positions
   
 end
